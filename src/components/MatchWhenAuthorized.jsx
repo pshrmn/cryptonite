5441443a02411bd450047cb2f1179cc915577751
Paul Sherman <paul.sherman.88@gmail.com>
2016-09-18 01:54:06 -0500

    remove a console.log

--- src/components/MatchWhenAuthorized.jsx
+++ src/components/MatchWhenAuthorized.jsx
@@ -2,19 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Match, Redirect } from 'react-router';
 
-const MatchWhenAuthorized = (props) => {
-  console.log(props);
-  const {component: Component, user, ...rest} = props;
-  return (
-    <Match {...rest} render={props => (
-      user && user.authenticated ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to={{ pathname: '/login', state: {from: props.location} }} />
-      )
-    )} />
-  );
-}
+const MatchWhenAuthorized = ({component: Component, user, ...rest}) => (
+  <Match {...rest} render={props => (
+    user && user.authenticated ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to={{ pathname: '/login', state: {from: props.location} }} />
+    )
+  )} />
+);
 
 export default connect(
   state => ({
