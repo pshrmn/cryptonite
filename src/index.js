ef285440bace1d96b54fd6c3de2c8b31b61e59c6
Paul Sherman <paul.sherman.88@gmail.com>
2018-02-06 02:12:53 -0600

    Update curi, render response handler

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ApolloClient, ApolloProvider, createNetworkInterface } from 'react-apollo';
-
 import Browser from '@hickory/browser';
 import curi from '@curi/core';
 import { parse, stringify } from 'qs';
 
-import client from './apolloClient';
 import routes from './routes';
-import renderFunction from './renderFunction';
-import ResponsiveBase from './components/ResponsiveBase';
+import renderApp from './render';
 
 const history = Browser({
   query: { parse, stringify }
 });
-
 const router = curi(history, routes);
-const root = document.querySelector('#app-holder');
-
-router.respond((response, action) => {
-  ReactDOM.render((
-    <ApolloProvider client={client}>
-      <ResponsiveBase router={router} render={renderFunction} />
-    </ApolloProvider>
-  ), root);
-});
+router.respond(renderApp);
---
+++ src/render.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { CuriProvider } from '@curi/react';
+
+import client from './apolloClient';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import 'scss/base.scss';
+import 'scss/main.scss';
+
+export default ({ router }) => {
+  ReactDOM.render((
+    <ApolloProvider client={client}>
+      <CuriProvider router={router}>
+        {({ response }) => {
+            const { body:Body, params, query, location } = response;
+            return (
+              <div id='app-base'>
+                <Header />
+                <main>
+                  <div className='container'>
+                    <Body params={params} query={query} location={location} />
+                  </div>
+                </main>
+                <Footer />
+              </div>
+            );
+          }
+          
+        }
+      </CuriProvider>
+    </ApolloProvider>
+  ), document.querySelector('#app-holder'));
+};
